f19ae8c14e08c30b0cc7329087718ae2c748fef1
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2025-04-05 01:54:53 -0600

    Realizacion del footer del sitio, reubicacion del atributo letter-spacing al body, declaracion de nuevos atributos en global.scss, al igual que el commit anterior, se incluyo el componente Link de NextJS para enlaces

--- pages/components/footer/index.jsx
+++ pages/components/footer/index.jsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return (
+      <Fragment>
+        <div className="borde-hr1"></div>
+          <div className="card border-0 text-color4">
+            <div className="card-body p-0 m-0">
+              <div className="d-flex flex-column flex-column-reverse flex-md-row">
+                <div className="py-4 py-lg-5 tc-cuatro-b align-items-center flex-fill text-center text-md-start">© 2025 Shield Plus. All Rights Reserved.</div>
+                <div className="borde-hr2"></div>
+                <div className="d-flex flex-row justify-content-between justify-content-lg-evenly py-4 py-lg-5 tc-cuatro-a align-items-center flex-fill">
+                  <Link className="nav-link" href="/">
+                    Terms & Conditions
+                  </Link>
+                  <Link className="nav-link" href="/">
+                    Privacy Policy
+                  </Link>
+                  <Link className="nav-link" href="/">
+                    Licensing
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+      </Fragment>
+    );
   }
